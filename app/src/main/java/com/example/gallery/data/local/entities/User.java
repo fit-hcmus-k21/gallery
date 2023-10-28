cca30e2d2f7c6ca71f70ec0ac78e5502d1a2eb05
thanhngan22 <nganbui9a2017@gmail.com>
2023-10-29 00:57:09 +0700

    add getters and setters for entities and add index unique for foreign key in album table

--- app/src/main/java/com/example/gallery/data/local/entities/User.java
+++ app/src/main/java/com/example/gallery/data/local/entities/User.java
@@ -43,4 +43,86 @@ public class User  {
     @ColumnInfo(name = "fingerprintData")
     private String fingerprintData;
 
+    // setters and getters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAvatarURL() {
+        return avatarURL;
+    }
+
+    public void setAvatarURL(String avatarURL) {
+        this.avatarURL = avatarURL;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getGoogleToken() {
+        return googleToken;
+    }
+
+    public void setGoogleToken(String googleToken) {
+        this.googleToken = googleToken;
+    }
+
+    public String getFacebookToken() {
+        return facebookToken;
+    }
+
+    public void setFacebookToken(String facebookToken) {
+        this.facebookToken = facebookToken;
+    }
+
+    public String getFingerprintData() {
+        return fingerprintData;
+    }
+
+    public void setFingerprintData(String fingerprintData) {
+        this.fingerprintData = fingerprintData;
+    }
 }
