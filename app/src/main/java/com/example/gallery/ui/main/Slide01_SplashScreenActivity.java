650190fbfee2703e8be59f1f0fa1ede65ca581ac
thanhngan22 <nganbui9a2017@gmail.com>
2023-10-29 15:10:48 +0700

    UI/ add login screen - slide 02

--- app/src/main/java/com/example/gallery/ui/main/Slide01_SplashScreenActivity.java
+++ app/src/main/java/com/example/gallery/ui/main/Slide01_SplashScreenActivity.java
@@ -11,25 +11,25 @@ import androidx.annotation.Nullable;
 
 import com.example.gallery.R;
 
-public class MainActivity extends Activity {
+public class Slide01_SplashScreenActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.slide01_splash_screen);
+        setContentView(R.layout.slide02_login_screen);
 
-        // set transition for gallery icon
-        ImageView rotatingImageView = findViewById(R.id.gallery_icon);
-
-        RotateAnimation rotateAnimation = new RotateAnimation(0, 360,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF, 0.5f);
-        rotateAnimation.setInterpolator(new LinearInterpolator());
-        rotateAnimation.setDuration(2000); //  (milliseconds)
-//        rotateAnimation.setRepeatMode(Animation.REVERSE);
-        rotateAnimation.setRepeatCount(Animation.INFINITE);
-
-        rotatingImageView.startAnimation(rotateAnimation);
+//        // set transition for gallery icon
+//        ImageView rotatingImageView = findViewById(R.id.gallery_icon);
+//
+//        RotateAnimation rotateAnimation = new RotateAnimation(0, 360,
+//                Animation.RELATIVE_TO_SELF, 0.5f,
+//                Animation.RELATIVE_TO_SELF, 0.5f);
+//        rotateAnimation.setInterpolator(new LinearInterpolator());
+//        rotateAnimation.setDuration(2000); //  (milliseconds)
+////        rotateAnimation.setRepeatMode(Animation.REVERSE);
+//        rotateAnimation.setRepeatCount(Animation.INFINITE);
+//
+//        rotatingImageView.startAnimation(rotateAnimation);
 
     }
 }
